e1a58f6e780105a383459b0b14547eb71129b0c3
DnestreanschiiMaxim <dnestreanschiimaxim780@gmail.com>
2022-07-08 17:47:09 +0300

    Contains the structures of the project

---
+++ struct.cpp
@@ -0,0 +1,20 @@
+struct user
+{
+	char name[256];
+	char email[256];
+	char password[50];
+	int l = 0;
+}U[100];
+
+struct stud
+{
+	char nume[256];
+	int ani;
+	double media;
+	int note[100];
+	int nrnote=0;
+
+}S[100];
+
+char FNS[256];
+char FNL[256];
